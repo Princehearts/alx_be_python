e220edab214d3a6491fe7e6cfb01a06f652b6d08
Princehearts <princeheartdyke@gmail.com>
2024-10-03 20:50:29 +0100

    This is my python introduction task

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,8 @@
+number1 = 10
+number2 = 5
+sum = number1 + number2
+diff = number1 - number2
+mul = number1 * number2
+print("Addition of " + str(number1) + " and " + str(number2)+ " is "+ str(sum))
+print("Subtraction of " + str(number1) + " and " + str(number2)+ " is "+ str(diff))
+print("Multiplication of " + str(number1) + " and " + str(number2)+ " is "+ str(mul))
