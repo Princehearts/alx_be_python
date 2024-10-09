6cec6be664ace32c00be52e420eef04c39e5b857
Princehearts <princeheartdyke@gmail.com>
2024-10-09 17:21:08 +0100

    Created a python multiplication table

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,4 +1,4 @@
-number = int(input("Enter a number to see its multiplication table (1, 10): "))
-for number in range (1, 10):
-    print(number)
-    print("X * Y = Z")
+number = int(input("Enter a number to see its multiplication table: "))
+for i in range (1, 10):
+    product = number * i
+    print(f"{number} * {i} = {product}")
